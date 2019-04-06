d18c91057901f82b7cf38156fcfb2994e22dad56
Romain Franceschini <franceschini.romain@gmail.com>
2019-04-06 14:30:04 +0200

    compare wrapper with raw calls to LibLMDB

--- examples/bm.cr
+++ examples/bm.cr
@@ -11,21 +11,50 @@ Dir.mkdir_p(path)
 puts GC.stats
 
 LMDB.open(path, LMDB.env_flags(NoSync, NoTls), map_size: 256 * 1024 * 1024, max_dbs: 10) do |env|
-  db = env.open_db("data", LMDB.db_flags(Create, IntegerKey))
+  db = env.database("wrapper", LMDB.db_flags(Create, IntegerKey))
+  db2 = env.database("raw", LMDB.db_flags(Create, IntegerKey))
 
   Benchmark.bm do |x|
     x.report("writes") do
-      count.times do |i|
+      (0..count).each do |i|
         env.transaction(on: db) { db[i] = i }
+      end
+    end
+
+    x.report("raw writes") do
+      (0..count).each do |i|
+        LibLMDB.txn_begin(env, nil, 0, out txn)
+        key = LibLMDB::Val.new
+        key.size = sizeof(Int32)
+        key.data = pointerof(i).as(UInt8*)
+        val = LibLMDB::Val.new
+        val.size = sizeof(Int32)
+        val.data = pointerof(i).as(UInt8*)
+        LibLMDB.put(txn, db2, pointerof(key), pointerof(val), 0)
+        LibLMDB.txn_commit(txn)
       end
     end
 
     x.report("batch writes") do
       env.transaction(on: db) do
-        count.times do |i|
+        (count..count*2).each do |i|
           db[i] = i
         end
       end
+    end
+
+    x.report("raw batch writes") do
+      LibLMDB.txn_begin(env, nil, 0, out txn)
+      (count..count*2).each do |i|
+        key = LibLMDB::Val.new
+        key.size = sizeof(Int32)
+        key.data = pointerof(i).as(UInt8*)
+        val = LibLMDB::Val.new
+        val.size = sizeof(Int32)
+        val.data = pointerof(i).as(UInt8*)
+        LibLMDB.put(txn, db2, pointerof(key), pointerof(val), 0)
+      end
+      LibLMDB.txn_commit(txn)
     end
   end
 
